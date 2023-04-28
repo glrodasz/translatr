545285133193a05e1d885ddad4df18325d344f74
Guillermo Rodas <me@guillermorodas.com>
2023-04-28 03:18:51 +0200

    Paso 13

---
+++ pages/api/translate.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+import { MAX_COUNT } from "@/utils/constants";
+
+const TOKEN_FACTOR = 4;
+const MAX_TOKENS = (MAX_COUNT / TOKEN_FACTOR) * 3;
+const GTP_MODEL = "gpt-3.5-turbo";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { text } = req.body;
+
+  if (req.method === "POST") {
+    try {
+      const response = await openai.createChatCompletion({
+        model: GTP_MODEL,
+        messages: [{ role: "user", content: `Traduce de Español a Ingles: ${text}` }],
+        temperature: 0,
+        max_tokens: MAX_TOKENS,
+      });
+
+      const translatedText =
+        response.data?.choices?.[0]?.message?.content?.trim() ?? "";
+
+      res.status(200).json({ translatedText });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "Something went wrong with the translation." });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
+
+export default handler;

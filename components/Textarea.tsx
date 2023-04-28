d373762916db95d2b64f6907db5d666a2d2d7e7a
Guillermo Rodas <me@guillermorodas.com>
2023-04-28 02:37:00 +0200

    Paso 6

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
 interface TextareaProps {
   placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   readOnly?: boolean;
 }
 
-const Textarea: React.FC<TextareaProps> = ({ placeholder, readOnly }) => {
+const Textarea: React.FC<TextareaProps> = ({
+  placeholder,
+  value,
+  onChange,
+  readOnly,
+}) => {
   return (
     <textarea
       className="w-full mt-4 p-4 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded resize-none"
       rows={10}
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
       readOnly={readOnly}
     ></textarea>
